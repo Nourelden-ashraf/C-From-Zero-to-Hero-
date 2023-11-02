892a22ba55814782129ca7a2b945235ab904ca0d
unknown <n.ashraf956@gmail.com>
2023-11-03 00:41:05 +0200

    1st Project

---
+++ Seconds_To_Minutes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float Seconds, minutes;
+    cout << "Enter Seconds";
+    cin >> Seconds;
+    minutes = Seconds / 60; //  The Mathmetical Equation
+    cout << minutes ; 
+    return 0; // End of Program
+}
