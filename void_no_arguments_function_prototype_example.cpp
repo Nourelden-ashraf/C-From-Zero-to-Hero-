e66e13567311e27c767cbf360d3eb79e57912660
nourelden-ashraf <n.ashraf956@gmail.com>
2023-11-19 04:04:09 +0200

    useless shit ngl

---
+++ void_no_arguments_function_prototype_example.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main () {
+    void bate5() ;{
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+        cout << "GOOOOOOOO bate5"<< endl;
+    }
+        void mangah() ;{
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+        cout << "GOOOOOOOO manga"<< endl;
+    };
+return 0;
+}
